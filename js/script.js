82c0d1a4f1648d56ad5d0c8c7c99eb269b5b031e
Dominyck Malta <106705509+nyckmlt@users.noreply.github.com>
2023-03-31 08:49:41 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//Feito por MANOEL MOREIRA
+function funcao1(){
+    var x; 
+    var r = confirm ('Site Fora do Ar Por Falta de Apoio Financeiro aos Devs');
+    if (r==true)
+    {
+        x ="Site Fora do Ar por Falta de Apoio financeiro aos Devs"
+    }
+    else{
+        x= "Site Fora do Ar por Falta de Apoio financeiro aos Devs"
+    }
+    
+}
